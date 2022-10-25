4e3107cf3ee3198eb6ede9b3f6d40bdaa524d47b
Gempur Bayu Aji <aji123zz@gmail.com>
2022-10-25 17:42:49 +0700

    delete data picture

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { GetInsight } from "./store/actions/InsightAction";
 import { RootStore } from "./store";
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
-
 const navList = [
   {
     title: "Home",
@@ -166,13 +159,13 @@ function App() {
             <MenuIcon className="h-6 w-6 text-cyan-900" />
           </div>
           <div className="flex items-center">
-            <div>
+            {/**<div>
               <img
                 className="w-8 h-8 rounded-full"
                 src={user.imageUrl}
                 alt="avatar"
               />
-            </div>
+          </div>*/}
             <LogoutIcon
               className="h-6 w-6 ml-4 text-cyan-700 cursor-pointer"
               onClick={wrapAsyncFunction(handleLogout)}
